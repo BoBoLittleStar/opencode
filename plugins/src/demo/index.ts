9bf751f65f0e6f1dedccd5a6804060457d914447
BoBoLittleStar <2467568493@qq.com>
2026-04-18 22:07:33 +0800

    restart

--- plugins/src/demo/index.ts
+++ plugins/src/demo/index.ts
@@ -2,7 +2,7 @@ import { Plugin, tool } from "@opencode-ai/plugin";
 import { getLogger } from "../libs/logger";
 
 export const Demo: Plugin = async () => {
-    getLogger().info("[Demo] Plugin loaded! Version 1.2.8");
+    getLogger().info("[Demo] Plugin loaded! Version 1.3.3");
 
     return {
         tool: {
@@ -10,7 +10,7 @@ export const Demo: Plugin = async () => {
                 description: "Get the demo plugin version",
                 args: {},
                 async execute() {
-                    return "1.2.8";
+                    return "1.3.3";
                 },
             }),
         },
